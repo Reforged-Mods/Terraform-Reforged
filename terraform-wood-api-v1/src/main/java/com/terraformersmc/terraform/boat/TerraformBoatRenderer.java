065740adcd621e0ce7fdc71064046cfd9f5d865b
Trinsdar <30245301+Trinsdar@users.noreply.github.com>
2021-08-31 14:00:38 -0400

    fixed boat renderer

--- terraform-wood-api-v1/src/main/java/com/terraformersmc/terraform/boat/TerraformBoatRenderer.java
+++ terraform-wood-api-v1/src/main/java/com/terraformersmc/terraform/boat/TerraformBoatRenderer.java
@@ -23,29 +23,29 @@ public class TerraformBoatRenderer extends EntityRenderer<TerraformBoatEntity> {
 	public void render(TerraformBoatEntity entityIn, float entityYaw, float partialTicks, MatrixStack matrixStackIn, VertexConsumerProvider bufferIn, int packedLightIn) {
 		matrixStackIn.push();
 		matrixStackIn.translate(0.0D, 0.375D, 0.0D);
-		matrixStackIn.multiply(Vector3f.YP.rotationDegrees(180.0F - entityYaw));
-		float f = (float) entityIn.getHurtTime() - partialTicks;
-		float f1 = entityIn.getDamage() - partialTicks;
+		matrixStackIn.multiply(Vector3f.POSITIVE_Y.getDegreesQuaternion(180.0F - entityYaw));
+		float f = (float) entityIn.getDamageWobbleTicks() - partialTicks;
+		float f1 = entityIn.getDamageWobbleStrength() - partialTicks;
 		if (f1 < 0.0F) {
 			f1 = 0.0F;
 		}
 
 		if (f > 0.0F) {
-			matrixStackIn.multiply(Vector3f.XP.rotationDegrees(MathHelper.sin(f) * f * f1 / 10.0F * (float) entityIn.getHurtDir()));
+			matrixStackIn.multiply(Vector3f.POSITIVE_X.getDegreesQuaternion(MathHelper.sin(f) * f * f1 / 10.0F * (float) entityIn.getDamageWobbleSide()));
 		}
 
-		float f2 = entityIn.getBubbleAngle(partialTicks);
-		if (!MathHelper.equal(f2, 0.0F)) {
-			matrixStackIn.multiply(new Quaternion(new Vector3f(1.0F, 0.0F, 1.0F), entityIn.getBubbleAngle(partialTicks), true));
+		float f2 = entityIn.interpolateBubbleWobble(partialTicks);
+		if (!MathHelper.approximatelyEquals(f2, 0.0F)) {
+			matrixStackIn.multiply(new Quaternion(new Vector3f(1.0F, 0.0F, 1.0F), entityIn.interpolateBubbleWobble(partialTicks), true));
 		}
 
 		matrixStackIn.scale(-1.0F, -1.0F, 1.0F);
-		matrixStackIn.multiply(Vector3f.YP.rotationDegrees(90.0F));
-		this.modelBoat.setupAnim(entityIn, partialTicks, 0.0F, -0.1F, 0.0F, 0.0F);
-		VertexConsumer ivertexbuilder = bufferIn.getBuffer(this.modelBoat.renderType(this.getTextureLocation(entityIn)));
-		this.modelBoat.renderToBuffer(matrixStackIn, ivertexbuilder, packedLightIn, OverlayTexture.NO_OVERLAY, 1.0F, 1.0F, 1.0F, 1.0F);
+		matrixStackIn.multiply(Vector3f.POSITIVE_Y.getDegreesQuaternion(90.0F));
+		this.modelBoat.setAngles(entityIn, partialTicks, 0.0F, -0.1F, 0.0F, 0.0F);
+		VertexConsumer ivertexbuilder = bufferIn.getBuffer(this.modelBoat.getLayer(this.getTexture(entityIn)));
+		this.modelBoat.render(matrixStackIn, ivertexbuilder, packedLightIn, OverlayTexture.DEFAULT_UV, 1.0F, 1.0F, 1.0F, 1.0F);
 		VertexConsumer ivertexbuilder1 = bufferIn.getBuffer(RenderLayer.getWaterMask());
-		this.modelBoat.waterPatch().render(matrixStackIn, ivertexbuilder1, packedLightIn, OverlayTexture.NO_OVERLAY);
+		this.modelBoat.getBottom().render(matrixStackIn, ivertexbuilder1, packedLightIn, OverlayTexture.DEFAULT_UV);
 		matrixStackIn.pop();
 		super.render(entityIn, entityYaw, partialTicks, matrixStackIn, bufferIn, packedLightIn);
 	}
