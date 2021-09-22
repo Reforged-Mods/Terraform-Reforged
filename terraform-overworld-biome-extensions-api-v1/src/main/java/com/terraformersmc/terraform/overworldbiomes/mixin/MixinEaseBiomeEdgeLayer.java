27b35488904205584a280a5b8d9edf1124925444
Trinsdar <30245301+Trinsdar@users.noreply.github.com>
2021-09-21 22:10:23 -0400

    ranamed methods in mixin

--- terraform-overworld-biome-extensions-api-v1/src/main/java/com/terraformersmc/terraform/overworldbiomes/mixin/MixinEaseBiomeEdgeLayer.java
+++ terraform-overworld-biome-extensions-api-v1/src/main/java/com/terraformersmc/terraform/overworldbiomes/mixin/MixinEaseBiomeEdgeLayer.java
@@ -21,43 +21,43 @@ import net.minecraft.world.biome.layer.util.LayerRandomnessSource;
 @SuppressWarnings("unused") // it's a mixin
 public class MixinEaseBiomeEdgeLayer {
 	@Inject(method = "sample", at = @At("HEAD"), cancellable = true)
-	public void onSample(LayerRandomnessSource rand, int neighbor1, int neighbor2, int neighbor3, int neighbor4, int center, CallbackInfoReturnable<Integer> info) {
+	public void terraform_onSample(LayerRandomnessSource rand, int neighbor1, int neighbor2, int neighbor3, int neighbor4, int center, CallbackInfoReturnable<Integer> info) {
 		//predicated borders
-		RegistryKey<Biome> biome = fromRawId(center);
+		RegistryKey<Biome> biome = terraform_fromRawId(center);
 
 		for (OverworldBiomesExt.PredicatedBiomeEntry entry : OverworldBiomesExt.getPredicatedBorders(biome)) {
-			if (entry.predicate.test(fromRawId(neighbor1)) || entry.predicate.test(fromRawId(neighbor2)) || entry.predicate.test(fromRawId(neighbor3)) || entry.predicate.test(fromRawId(neighbor4))) {
-				info.setReturnValue(toRawId(entry.biome));
+			if (entry.predicate.test(terraform_fromRawId(neighbor1)) || entry.predicate.test(terraform_fromRawId(neighbor2)) || entry.predicate.test(terraform_fromRawId(neighbor3)) || entry.predicate.test(terraform_fromRawId(neighbor4))) {
+				info.setReturnValue(terraform_toRawId(entry.biome));
 			}
 		}
 
 		//border biomes
 		boolean replaced =
-				tryReplace(center, neighbor1, info::setReturnValue) ||
-						tryReplace(center, neighbor2, info::setReturnValue) ||
-						tryReplace(center, neighbor3, info::setReturnValue) ||
-						tryReplace(center, neighbor4, info::setReturnValue);
+				terraform_tryReplace(center, neighbor1, info::setReturnValue) ||
+						terraform_tryReplace(center, neighbor2, info::setReturnValue) ||
+						terraform_tryReplace(center, neighbor3, info::setReturnValue) ||
+						terraform_tryReplace(center, neighbor4, info::setReturnValue);
 
 		if (replaced) {
 			return;
 		}
 
 		//center biomes
-		if (surrounded(neighbor1, neighbor2, neighbor3, neighbor4, center)) {
-			Optional<RegistryKey<Biome>> target = OverworldBiomesExt.getCenter(fromRawId(center));
+		if (terraform_surrounded(neighbor1, neighbor2, neighbor3, neighbor4, center)) {
+			Optional<RegistryKey<Biome>> target = OverworldBiomesExt.getCenter(terraform_fromRawId(center));
 
-			target.ifPresent(value -> info.setReturnValue(toRawId(value)));
+			target.ifPresent(value -> info.setReturnValue(terraform_toRawId(value)));
 		}
 	}
 
-	private static boolean tryReplace(int center, int neighbor, IntConsumer consumer) {
+	private static boolean terraform_tryReplace(int center, int neighbor, IntConsumer consumer) {
 		if (center == neighbor) {
 			return false;
 		}
 
-		Optional<RegistryKey<Biome>> border = OverworldBiomesExt.getBorder(fromRawId(neighbor));
+		Optional<RegistryKey<Biome>> border = OverworldBiomesExt.getBorder(terraform_fromRawId(neighbor));
 		if (border.isPresent()) {
-			consumer.accept(toRawId(border.get()));
+			consumer.accept(terraform_toRawId(border.get()));
 
 			return true;
 		}
@@ -65,19 +65,19 @@ public class MixinEaseBiomeEdgeLayer {
 		return false;
 	}
 
-	private static boolean surrounded(int neighbor1, int neighbor2, int neighbor3, int neighbor4, int biome) {
+	private static boolean terraform_surrounded(int neighbor1, int neighbor2, int neighbor3, int neighbor4, int biome) {
 		return neighbor1 == biome && neighbor2 == biome && neighbor3 == biome && neighbor4 == biome;
 	}
 
-	private static RegistryKey<Biome> fromRawId(int raw) {
+	private static RegistryKey<Biome> terraform_fromRawId(int raw) {
 		return BuiltinBiomes.fromRawId(raw);
 	}
 
-	private static int toRawId(RegistryKey<Biome> key) {
-		return BuiltinRegistries.BIOME.getRawId(getOrThrow(key));
+	private static int terraform_toRawId(RegistryKey<Biome> key) {
+		return BuiltinRegistries.BIOME.getRawId(terraform_getOrThrow(key));
 	}
 
-	private static Biome getOrThrow(RegistryKey<Biome> key){
+	private static Biome terraform_getOrThrow(RegistryKey<Biome> key){
 		if (ForgeRegistries.BIOMES.containsKey(key.getValue())){
 			return ForgeRegistries.BIOMES.getValue(key.getValue());
 		}
