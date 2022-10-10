a33812aec402e9594b5bbffa851121b3cd818bba
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2022-10-09 21:45:33 -0400

    fixed modid provided in shapes api mod class

--- terraform-shapes-api-v1/src/main/java/com/terraformersmc/terraform/shapes/TerraformShapeApi.java
+++ terraform-shapes-api-v1/src/main/java/com/terraformersmc/terraform/shapes/TerraformShapeApi.java
@@ -2,6 +2,6 @@ package com.terraformersmc.terraform.shapes;
 
 import net.minecraftforge.fml.common.Mod;
 
-@Mod("terraform_shape_api_v1")
+@Mod("terraform_shapes_api_v1")
 public class TerraformShapeApi {
 }
