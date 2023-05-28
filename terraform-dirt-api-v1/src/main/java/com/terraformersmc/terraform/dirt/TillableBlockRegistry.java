16f6fff113c29de15bf0e7ed6752d6da501199cb
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2023-05-27 22:40:12 -0400

    fixed hoe registry

--- terraform-dirt-api-v1/src/main/java/com/terraformersmc/terraform/dirt/TillableBlockRegistry.java
+++ terraform-dirt-api-v1/src/main/java/com/terraformersmc/terraform/dirt/TillableBlockRegistry.java
@@ -1,5 +1,7 @@
 package com.terraformersmc.terraform.dirt;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.function.Consumer;
 import java.util.function.Predicate;
 
@@ -10,15 +12,19 @@ import net.minecraft.block.BlockState;
 import net.minecraft.item.HoeItem;
 import net.minecraft.item.ItemUsageContext;
 import net.minecraft.item.ToolMaterial;
+import net.minecraftforge.common.ToolAction;
+import net.minecraftforge.common.ToolActions;
+import net.minecraftforge.event.world.BlockEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
 
 /**
  * Allows the addition of custom tillable block mappings. You probably don't need to use this directly if you're using
  * {@link TerraformDirtRegistry}.
  */
-public abstract class TillableBlockRegistry extends HoeItem {
-	private TillableBlockRegistry(ToolMaterial material, int attackDamage, float attackSpeed, Settings settings) {
-		super(material, attackDamage, attackSpeed, settings);
-	}
+@Mod.EventBusSubscriber(modid = "terraform_dirt_api_v1")
+public abstract class TillableBlockRegistry {
+	protected static final Map<Block, BlockState> TILLING_ACTIONS = new HashMap<>();
 
 	/**
 	 * Adds a custom tillable block mapping.
@@ -29,7 +35,7 @@ public abstract class TillableBlockRegistry extends HoeItem {
 	 * @param pair the interaction between the blocks
 	 */
 	public static void add(Block block, Pair<Predicate<ItemUsageContext>, Consumer<ItemUsageContext>> pair) {
-		TILLING_ACTIONS.put(block, pair);
+		//TILLING_ACTIONS.put(block, pair);
 	}
 	
 	/**
@@ -41,6 +47,17 @@ public abstract class TillableBlockRegistry extends HoeItem {
 	 * @param state the block to be replaced with
 	 */
 	public static void add(Block block, BlockState state) {
-		TILLING_ACTIONS.put(block, Pair.of(HoeItem::canTillFarmland, createTillAction(state)));
+		//TILLING_ACTIONS.put(block, Pair.of(HoeItem::canTillFarmland, HoeItem.createTillAction(state)));
+		TILLING_ACTIONS.put(block, state);
+	}
+
+	@SubscribeEvent
+	public static void onBlockToolInteraction(BlockEvent.BlockToolModificationEvent event){
+		if (event.getToolAction() == ToolActions.HOE_TILL){
+			if (TILLING_ACTIONS.containsKey(event.getFinalState().getBlock())){
+				BlockState state = TILLING_ACTIONS.get(event.getFinalState().getBlock());
+				event.setFinalState(state);
+			}
+		}
 	}
 }
