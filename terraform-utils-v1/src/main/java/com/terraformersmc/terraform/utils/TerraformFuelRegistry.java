92066dc5997042e31d25b80bcc89df808ba16562
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2023-05-28 21:07:00 -0400

    added addFuel method for blocks

--- terraform-utils-v1/src/main/java/com/terraformersmc/terraform/utils/TerraformFuelRegistry.java
+++ terraform-utils-v1/src/main/java/com/terraformersmc/terraform/utils/TerraformFuelRegistry.java
@@ -1,6 +1,7 @@
 package com.terraformersmc.terraform.utils;
 
 import it.unimi.dsi.fastutil.objects.Object2ObjectArrayMap;
+import net.minecraft.block.Block;
 import net.minecraft.item.Item;
 
 import java.util.Map;
@@ -10,5 +11,9 @@ public class TerraformFuelRegistry {
 
 	public static void addFuel(Item item, int fuelValue){
 		FUEL_MAP.put(item, fuelValue);
+	}
+
+	public static void addFuel(Block block, int fuelValue){
+		FUEL_MAP.put(block.asItem(), fuelValue);
 	}
 }
